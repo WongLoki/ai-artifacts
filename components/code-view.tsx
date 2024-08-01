b72f13867bd5dd485f095cafed572f20171f657b
Loki <lokiw@msn.com>
2024-08-01 15:26:22 +0000

    fix

--- components/code-view.tsx
+++ components/code-view.tsx
@@ -1,10 +1,12 @@
 import Editor from 'react-simple-code-editor'
-import Prism from 'prismjs'
+import { highlight, languages } from 'prismjs/components/prism-core'
 import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-python'
-import 'prismjs/themes/prism.css' // Example style, you can use another
+import 'prismjs/themes/prism.css' //Example style, you can use another
+
 import { SandboxTemplate } from '@/lib/types'
+
 
 export function CodeView({ code, template }: { code: string, template: SandboxTemplate }) {
   return (
@@ -12,11 +14,7 @@ export function CodeView({ code, template }: { code: string, template: SandboxTe
       value={code}
       disabled={true}
       onValueChange={() => {}}
-      highlight={code => Prism.highlight(
-        code, 
-        template === SandboxTemplate.CodeInterpreterMultilang ? Prism.languages.python : Prism.languages.javascript, 
-        template === SandboxTemplate.CodeInterpreterMultilang ? 'python' : 'javascript'
-      )}
+      highlight={code => highlight(code, template === SandboxTemplate.CodeInterpreterMultilang ? languages.python : languages.javascript)}
       padding={10}
       style={{
         fontFamily: '"Fira code", "Fira Mono", monospace',
